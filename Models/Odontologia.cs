61c1a59bca88466c28dba2ac1f625b9ad743c2f4
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2023-11-18 19:17:31 -0500

    Model Odontologia y Urgencia

---
+++ Models/Odontologia.cs
@@ -0,0 +1,12 @@
+﻿namespace CiudadanosSanos.Models
+{
+    public class Odontologia
+    {
+        public int Id { get; set; }
+        public int Documento { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string? Descripcion { get; set; }
+        public ICollection<Product>? Products { get; set; } = default!;
+    }
+}
