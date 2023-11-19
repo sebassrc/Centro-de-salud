099ff81eae52b135ed061eed743d57451e109b96
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2023-11-18 19:16:26 -0500

    Model Hospitalizacion

---
+++ Models/Hospitalizacion.cs
@@ -0,0 +1,10 @@
+﻿namespace CiudadanosSanos.Models
+{
+    public class Hospitalizacion
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string? Descripcion { get; set; }
+        public ICollection<Product>? Products { get; set; } = default!;
+    }
+}
