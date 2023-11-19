d4153fd9c96d815c90481af1d7bcb1fbf8284b54
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2023-11-18 19:18:07 -0500

    Model Product

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CiudadanosSanos.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        public Consulta Category { get; set; }
+    }
+}
